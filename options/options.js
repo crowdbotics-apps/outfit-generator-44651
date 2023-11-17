ec4a0cda45e5e8bf71e74f7d11cbac6dcc7dc61d
Crowdbotics <team@crowdbotics.com>
2023-11-17 20:31:11 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "outfit-generator-44651",
+  url: "https://outfit-generator-44651.botics.co",
+  api: "https://outfit-generator-44651.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
